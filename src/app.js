50a4f2adf49994b1de06658759a27c61c97fcbac
schesnowitz <steve@chesnowitz.com>
2017-04-20 04:46:45 -0400

    base app

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import react { Component } from 'react';
+import { View, Text } from 'react-native';
+
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Authorization App</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
