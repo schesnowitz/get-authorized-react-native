853f594845a203301f027f850773140cf8864830
schesnowitz <steve@chesnowitz.com>
2017-04-21 10:08:35 -0400

    comp

--- src/app.js
+++ src/app.js
@@ -1,25 +1,50 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import firebase from 'firebase';
-import Header  from './components/Header'
-import LoginForm  from './components/LoginForm'
+import { Header, Button, Spinner } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
-    componentWillMount() {
-      firebase.initializeApp({
-      apiKey: "AIzaSyAzgLbvw_mcV2Ri_I3uyMU5l3z7VDeMA3o",
-      authDomain: "authentication-34e29.firebaseapp.com",
-      databaseURL: "https://authentication-34e29.firebaseio.com",
-      projectId: "authentication-34e29",
-      storageBucket: "authentication-34e29.appspot.com",
-      messagingSenderId: "200649221104"
+  state = { loggedIn: null };
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBtxMY4K6uHxv_2e3GD-FWAD2ACX6lPVRE',
+      authDomain: 'authentication-70a18.firebaseapp.com',
+      databaseURL: 'https://authentication-70a18.firebaseio.com',
+      storageBucket: 'authentication-70a18.appspot.com',
+      messagingSenderId: '682333809338'
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
     });
   }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Button onPress={() => firebase.auth().signOut()}>
+            Log Out
+          </Button>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
+  }
+
   render() {
     return (
       <View>
-        <Header headerText='This is the Header' />
-        <LoginForm />
+        <Header headerText="Authentication" />
+        {this.renderContent()}
       </View>
     );
   }
--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { TextInput, View, Text } from 'react-native';
 
-const InputField = ({ label, value, onChangeText, placeholder, secureTextEntry }) => {
+const Input = ({ label, value, onChangeText, placeholder, secureTextEntry }) => {
   const { inputStyle, labelStyle, containerStyle } = styles;
 
   return (
@@ -41,4 +41,4 @@ const styles = {
   }
 };
 
-export { InputField };
+export { Input };
